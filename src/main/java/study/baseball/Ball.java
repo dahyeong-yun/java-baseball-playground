e0afdff4eb777e036d587611437fe84a1724a893
윤다형 <dahyeoung.yoon@ta9.co.kr>
2022-04-08 16:45:32 +0900

    :white_check_mark: NOTHING 결과가 나오는 경우 개발

--- src/main/java/study/baseball/Ball.java
+++ src/main/java/study/baseball/Ball.java
@@ -2,24 +2,38 @@ package study.baseball;
 
 public class Ball {
 
-	private final int ballOrder;
-	private final int ballValue;
+    private final int ballOrder;
+    private final int ballValue;
 
-	private Ball(int ballOrder, int ballValue) {
-		this.ballOrder = ballOrder;
-		this.ballValue = ballValue;
-	}
+    private Ball(int ballOrder, int ballValue) {
+        this.ballOrder = ballOrder;
+        this.ballValue = ballValue;
+    }
 
-	public static Ball createBall(int ballOrder, int ballValue) {
-		return new Ball(ballOrder, ballValue);
-	}
+    public static Ball createBall(int ballOrder, int ballValue) {
+        return new Ball(ballOrder, ballValue);
+    }
 
-	public int getOrder() {
-		return this.ballOrder;
-	}
+    public int getOrder() {
+        return this.ballOrder;
+    }
 
-	public Integer getValue() {
-		return this.ballValue;
-	}
+    public Integer getValue() {
+        return this.ballValue;
+    }
 
+    private boolean isSameValue(int gameValue, int userValue) {
+        return gameValue == userValue;
+    }
+
+    private boolean isSameOrder(int gameOrder, int userOrder) {
+        return gameOrder == userOrder;
+    }
+    
+    public BallStatus play(Ball userBall) {
+        if(isSameOrder(this.ballOrder, userBall.getOrder()) && isSameValue(this.ballValue, userBall.ballValue)) {
+            return BallStatus.NOTHING;
+        }
+        return BallStatus.NOTHING;
+    }
 }
--- src/test/java/study/BallTest.java
+++ src/test/java/study/BallTest.java
@@ -6,17 +6,35 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import study.baseball.Ball;
+import study.baseball.BallStatus;
 
 class BallTest {
 
-	@Test
-	@DisplayName("공은 위치와 값을 같는다.")
-	void test() {
-		int ballOrder = 1;
-		int ballValue = 2;
-		
-		Ball ball = Ball.createBall(ballOrder, ballValue);
-		assertEquals(ballOrder, ball.getOrder());
-		assertEquals(ballValue, ball.getValue());
-	}
+    @Test
+    @DisplayName("공은 위치와 값을 같는다.")
+    void test() {
+        int ballOrder = 1;
+        int ballValue = 2;
+        
+        Ball ball = Ball.createBall(ballOrder, ballValue);
+        assertEquals(ballOrder, ball.getOrder());
+        assertEquals(ballValue, ball.getValue());
+    }
+    
+    // 어떤 입력이 있을 때, 순서와 값이 맞는지 비교한다.
+    // 입력을 통해 생성된 어떤 객체와 Ball 인스턴스를 비교한다.
+    // 그냥 같은 객체를 사용자와 게임으로 이름을 구분한다.
+    @Test
+    @DisplayName("공의 위치와 값이 다른 경우는 NOTHING 이다.")
+    void isBall() {
+        int gameValue = 4;
+        int userValue = 3;
+
+        int gameOrder = 2;
+        int userOrder = 1;
+        
+        Ball gameBall = Ball.createBall(gameOrder, gameValue);
+        Ball userBall = Ball.createBall(userOrder, userValue);
+        assertEquals(BallStatus.NOTHING, gameBall.play(userBall));
+    }
 }
